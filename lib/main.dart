558a6f065fa5fc1c4c9edd4d6c39c61ef0fb5970
tgog111 <gongsi2017@gmail.com>
2025-05-27 02:52:15 +0900

    확인중

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo goto 잘 commit 되는지 확인중 ',
       theme: ThemeData(
         // This is the theme of your application.
         //
